8c111bf60ea74276c622701d9d299f72693334b1
Astra <astroorbis@gmail.com>
2025-11-01 18:07:21 -0400

    rollback

--- pkgs/ryubing/default.nix
+++ pkgs/ryubing/default.nix
@@ -30,14 +30,14 @@
 
 buildDotnetModule rec {
   pname = "ryubing";
-  version = "1.3.203";
+  version = "1.3.193";
 
   src = fetchFromGitLab {
     domain = "git.ryujinx.app";
     owner = "Ryubing";
     repo = "Ryujinx";
     tag = "Canary-${version}";
-    hash = "sha256-QnKQEaz01HJVD2zV1Rv5Y2B2iOPOl65kK6X/7J0ccbk=";
+    hash = "sha256-mH2SF6J8AQtXFcjaux5Mh/mvuvPR/VDSCOKVHVrBtgQ=";
   };
 
   nativeBuildInputs = lib.optional stdenv.hostPlatform.isDarwin [
